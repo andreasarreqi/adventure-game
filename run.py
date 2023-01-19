2f8a531e33a1f58e69f118a0ffa0619498bb2b3c
Andre <sarreqiandrea@gmail.com>
2023-01-19 16:36:44 +0000

    Working on the game start function

--- run.py
+++ run.py
@@ -6,4 +6,31 @@ def game_introduction():
     """"
     Game intro
     """
-    
+    print()
+    print("Quite an exciting day for you passes by.\n")
+    print("You find yourself at the airport few minutes away from departing to your holiday destination.\n")
+    time.sleep(2)
+    print("Everything seems to be going well.\n")
+    print("The flight seems to be quiet and very comfy.\n")
+    time.sleep(2)
+    print(".")
+    print("Until..")
+    print(".")
+    print(".")
+    print(".")
+    print("One of the engines explodes...\n")
+    time.sleep(2)
+    print("After the pilot's struggle to keep the plane flying")
+    print("And using the radio for help he fails.")
+    print("Eventually the plane crashes\n")
+    time.sleep(2)
+    print("BOOM...")
+
+
+game_introduction()
+
+
+def game_start():
+    """
+    Setting up the game start function
+    """
