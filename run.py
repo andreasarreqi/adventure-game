2ff7ad610a32d201cd99dad7a13b93b771599067
Andre <sarreqiandrea@gmail.com>
2023-01-19 17:05:12 +0000

    Add the username function

--- run.py
+++ run.py
@@ -22,9 +22,11 @@ def game_introduction():
     time.sleep(2)
     print("After the pilot's struggle to keep the plane flying")
     print("And using the radio for help he fails.")
-    print("Eventually the plane crashes\n")
+    print("Eventually the plane crashes in a deserted island\n")
     time.sleep(2)
-    print("BOOM...")
+    print("BOOM...\n")
+    print(".")
+    print(".")
 
 
 game_introduction()
@@ -33,4 +35,38 @@ game_introduction()
 def game_start():
     """
     Setting up the game start function
+    """
+    print("After few hours of being unconcious, you wake up.")
+    print("Confused and lost, you try to look around the cabin")
+    print("You realise that you are the only one alive...")
+    while True:
+        find_the_way = input("Would you like to try to find a way " +
+        "to leave the island? (yes/no):\n")
+
+        if find_the_way == "no":
+            print("\nWell, it was nice knowing you...")
+            time.sleep(2)
+            print("Enjoy a slow and painful death from dehydration...\n")
+            time.sleep(3)
+            print("GAME OVER\n")
+            play_again()
+            break
+        if find_the_way == "yes":
+            get_username()
+            break
+        else:
+            print("Wrong input. Please type 'yes' or 'no'.")
+            continue
+
+
+game_start()
+
+
+def username():
+ """
+    Continues with the story, asks user to input a name. If no input,
+    user asked again. If input provided, game continues giving the user
+    three directions to choose from: right (second_choice function), left
+    (eightth_choice function), move forward (fourth_choice function). If incorrect input
+    provided, user is asked the question again.
     """
